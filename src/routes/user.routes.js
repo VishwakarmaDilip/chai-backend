64e9713268cb1e3eb8d51945fd8ae1eb530b12b3
Dilip Vishwakarma <2106dilip@gmail.com>
2025-03-10 17:11:21 +0530

    add new user-related routes for password change, current user retrieval, account updates, avatar and cover image updates, user channel profile, and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js"
-import {upload} from "../middlewares/multer.middleware.js"
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetail, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js"
+import { upload } from "../middlewares/multer.middleware.js"
 import { veryfyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
@@ -9,11 +9,11 @@ router.route("/register").post(
     upload.fields([
         {
             name: "avatar",
-            maxCount:1
+            maxCount: 1
         },
         {
             name: "coverImage",
-            maxCount:1
+            maxCount: 1
         }
     ]),
     registerUser
@@ -27,6 +27,19 @@ router.route("/logout").post(veryfyJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
 
+router.route("/change-password").post(veryfyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(veryfyJWT, getCurrentUser)
+
+router.route("/update-account").patch(veryfyJWT, updateAccountDetail)
+
+router.route("/avatar").patch(veryfyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/cover-image").patch(veryfyJWT, upload.single("/coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(veryfyJWT, getUserChannelProfile)
+
+router.route("/history").get(veryfyJWT, getWatchHistory)
 
 
 export default router
